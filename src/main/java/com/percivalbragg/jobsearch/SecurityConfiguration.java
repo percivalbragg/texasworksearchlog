4f72f92c1839a4a96ea76e826ebc04fda8489931
percivalbragg <percivalbragg@yahoo.com>
2020-11-23 16:10:28 -0600

    Added Spring Security to project

---
+++ src/main/java/com/percivalbragg/jobsearch/SecurityConfiguration.java
@@ -0,0 +1,38 @@
+package com.percivalbragg.jobsearch;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		
+		auth.inMemoryAuthentication()
+			.withUser("percivalbragg")
+			.password("percivalbragg")
+			.roles("ADMIN")
+			.and()
+			.withUser("percy")
+			.password("foo")
+			.roles("USER");
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		
+		http.authorizeRequests()
+			.antMatchers("/**").hasAnyRole("USER", "ADMIN")
+			.and().formLogin();
+	}
+
+	@Bean
+	public PasswordEncoder getPasswordEncoder() {return NoOpPasswordEncoder.getInstance();}
+
+}
